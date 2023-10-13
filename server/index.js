dec541bbbaeaabc2c88174b48ecc3c623dde05dc
MohamedAklamaash <aklamaash78@gmail.com>
2023-10-13 07:40:55 +0530

    made some changes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ const cors = require("cors");
 const payment = require("./routes/paymentRoute");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(
+  cors({
+    origin: "https://aklamaash-e-commerce-app.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.get("/statusCheck",(req,res)=>{
     return res.send("Hello World");
 })
