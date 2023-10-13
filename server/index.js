b8cf733343645b5ad26024f454e5ff89eac04e4b
MohamedAklamaash <aklamaash78@gmail.com>
2023-10-13 07:35:54 +0530

    need to check cors error

--- server/index.js
+++ server/index.js
@@ -21,6 +21,15 @@ app.use(
 app.get("/statusCheck",(req,res)=>{
     return res.send("Hello World");
 })
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://aklamaash-e-commerce-app.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(cookieParser());
 app.use("/api/products",product);
 app.use("/api/users",user);
